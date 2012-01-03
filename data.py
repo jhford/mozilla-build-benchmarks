8e816e6acb75d11a9c78952ac753941877698588
John Ford <john@johnford.info>
2012-01-03 10:00:33 -0500

    move file processing into a function

--- data.py
+++ data.py
@@ -3,9 +3,6 @@
 import csv
 import sys
 import StringIO
-import itertools
-
-
 
 def find_average_time(a,b):
     times=[]
@@ -13,29 +10,31 @@ def find_average_time(a,b):
         times.append(b[i] - a[i])
     return sum(times, 0.0) / len(times)
 
+def find_average_from_file(filename):
+    # Strip trailing commas
+    data_file = open(filename)
+    mem_data = StringIO.StringIO()
+    for line in data_file.readlines():
+        print >> mem_data, line.strip().rstrip(',')
+    data_file.close()
+
+    mem_data.seek(0)
+
+    reader = csv.reader(mem_data)
+
+    headers = reader.next()
+    data = dict([(x,[]) for x in headers])
+    for row in reader:
+        if len(row) == len(headers):
+            for i in range(0,len(headers)):
+                data[headers[i]].append(int(row[i]))
+
+    return headers, data
+
 if len(sys.argv) != 2:
     exit(1)
 
-# Strip trailing commas
-data_file = open(sys.argv[1])
-mem_data = StringIO.StringIO()
-for line in data_file.readlines():
-    print >> mem_data, line.strip().rstrip(',')
-data_file.close()
-
-mem_data.seek(0)
-
-reader = csv.reader(mem_data)
-
-headers = reader.next()
-data = dict([(x,[]) for x in headers])
-for row in reader:
-    if len(row) == len(headers):
-        for i in range(0,len(headers)):
-            data[headers[i]].append(int(row[i]))
-
-
-# Do stuff below here
+headers, data = find_average_from_file(sys.argv[1])
 
 print "Average end to end Time: ", find_average_time(data[headers[0]], data[headers[-1]])
 for i in range(1,len(headers)):
